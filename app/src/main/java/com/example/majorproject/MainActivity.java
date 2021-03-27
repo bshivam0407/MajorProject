c7d17fe45373a088ffda6eb93cba37024834cd85
shivambansal <bshivam0407@gmail.com>
2021-03-27 13:34:30 +0530

    Upload test images in compressed form

--- app/src/main/java/com/example/majorproject/MainActivity.java
+++ app/src/main/java/com/example/majorproject/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.majorproject;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.ClipData;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -15,26 +17,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Button uploadButton;
-    int PICK_IMAGE_MULTIPLE = 1;
-    String imageEncoded;
-    List<String> imagesEncodedList;
-    ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
-    private StorageReference storageRef;
-    private FirebaseApp app;
-    private FirebaseStorage storage;
+    private DatabaseReference databaseReference;
+    private  StorageReference storageReference;
+    private static  final  int PICK_IMAGE=1;
+    int imageCountFirebase=0;
+    ArrayList<Uri> ImageUriList = new ArrayList<>();
+    ArrayList<byte[]> ImageArray= new ArrayList<>();
 
 
     @Override
@@ -42,85 +50,109 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         uploadButton= (Button) findViewById(R.id.upload);
-        app = FirebaseApp.getInstance();
-        storage =FirebaseStorage.getInstance(app);
+
+        databaseReference =FirebaseDatabase.getInstance().getReference("MAJOR PROJECT");
+        storageReference = FirebaseStorage.getInstance().getReference("MAJOR PROJECT");
+
+        Log.i("LOG_TAG",Integer.toString(imageCountFirebase));
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent,"Select Picture"), PICK_IMAGE_MULTIPLE);
+                getImageCount();
+
+
             }
         });
+
     }
+    void uploadImage()
+    {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        startActivityForResult(intent, PICK_IMAGE);
+    }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        try {
-            // When an Image is picked
-            if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == RESULT_OK
-                    && null != data) {
-                // Get the Image from data
-
-                String[] filePathColumn = { MediaStore.Images.Media.DATA };
-                if(data.getData()!=null){
-
-                    Uri mImageUri=data.getData();
-
-                } else {
-                    if (data.getClipData() != null) {
-                        ClipData mClipData = data.getClipData();
-
-                        for (int i = 0; i < mClipData.getItemCount(); i++) {
-
-                            ClipData.Item item = mClipData.getItemAt(i);
-                            Uri uri = item.getUri();
-                            mArrayUri.add(uri);
-                        }
-                        uploadImages();
-                        Log.v("LOG_TAG", "Selected Images" + mArrayUri.size());
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==PICK_IMAGE && resultCode==RESULT_OK)
+        {
+            if(data.getClipData()!=null)
+            {
+                int count = data.getClipData().getItemCount();
+                for(int i=0;i<count;i++)
+                {
+                    Uri ImageUri = data.getClipData().getItemAt(i).getUri();
+                    Bitmap bitmap = null;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), ImageUri);
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
+                    int nh = (int) ( bitmap.getHeight() * (512.0 / bitmap.getWidth()) );
+                    Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 512, nh, true);
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    scaled.compress(Bitmap.CompressFormat.JPEG,50, stream);
+                    byte[] byteArray = stream.toByteArray();
+                    ImageUriList.add(ImageUri);
+                    ImageArray.add(byteArray);
                 }
-            } else {
-                Toast.makeText(this, "You haven't picked Image",
-                        Toast.LENGTH_LONG).show();
+                uploadImageToFirebase(0,count);
             }
-        } catch (Exception e) {
-            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
-                    .show();
         }
 
-        super.onActivityResult(requestCode, resultCode, data);
     }
-
-    void uploadImages()
-    {
-        ProgressDialog progressdialog = new ProgressDialog(getApplicationContext());
-        progressdialog.setMessage("Please Wait....");
-        storageRef=storage.getReference("Photos");
-        for(int i=0;i<mArrayUri.size();i++)
+    void uploadImageToFirebase(int i,int count){
+        if(i==count)
         {
-            Uri image = mArrayUri.get(i);
-            StorageReference imageRef=storageRef.child(image.getLastPathSegment());
-            storageRef.child(image.getLastPathSegment()).putFile(image).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            Toast.makeText(MainActivity.this,"Image Uploaded",Toast.LENGTH_SHORT).show();
+            databaseReference.child("ImageCount").setValue(imageCountFirebase);
+        }
+        else
+        {
+            final StorageReference ImageName =storageReference.child("Img" + Integer.toString(imageCountFirebase));
+            ImageName.putBytes(ImageArray.get(i)).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    ImageName.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                         @Override
                         public void onSuccess(Uri uri) {
-                            String url = uri.toString();
-                            Log.i("LOG_TAG",url);
+                            String url =uri.toString();
+                            databaseReference.child("Images").child(Integer.toString(imageCountFirebase)).child("id").setValue(url).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    imageCountFirebase++;
+                                    uploadImageToFirebase(i+1,count);
+                                }
+                            });
                         }
                     });
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(MainActivity.this,"Failed to upload",Toast.LENGTH_SHORT).show();
+
                 }
             });
         }
     }
+    void  getImageCount(){
+        databaseReference.child("ImageCount").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if(!task.isSuccessful())
+                {
+                    Toast.makeText(MainActivity.this,"Error, try again!",Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+                    String count= task.getResult().getValue().toString();
+                    imageCountFirebase =  Integer.parseInt(count);
+                    uploadImage();
+
+                }
+            }
+        });
+
+    }
+
 
 }
